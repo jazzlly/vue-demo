dc73d6d778b444feb53d19e446092e6da459a560
姜睿 <rui.jiang@pekall.com>
2019-03-05 09:28:55 +0800

    update!

---
+++ vue/cli-300-vuex/src/components/Counter.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <button class="btn btn-primary" @click="increment">Increment</button>
+        <button class="btn btn-primary" @click="decrement">Decrement</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        methods: {
+            increment() {
+                //this.$emit('updated', 1);
+                
+                // 直接改变状态也是可以的！
+                // this.$store.state.counter++;
+                this.$store.commit('increment');
+            },
+            decrement() {
+                // this.$emit('updated', -1);
+                
+                // 直接改变也是可以的！
+                // this.$store.state.counter--;
+                this.$store.commit('decrement')
+                
+            }
+        }
+    }
+</script>
