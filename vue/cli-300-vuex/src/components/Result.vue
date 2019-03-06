e476de1ab41f6e5e750a7b49bb76cc6bcbea8056
ryan <jiangrui.tr2008@gmail.com>
2019-03-06 09:44:51 +0800

    update!

--- vue/cli-300-vuex/src/components/Result.vue
+++ vue/cli-300-vuex/src/components/Result.vue
@@ -3,13 +3,19 @@
 </template>
 
 <script>
+    import { mapState } from 'vuex';
+
     export default {
         // props: ['counter'],
         
-        computed: {
-            counter() {
-                return this.$store.state.counter;
-            }
-        }   
+        // computed: {
+        //     counter() {
+        //         return this.$store.state.counter;
+        //     }
+        // }   
+
+        computed: mapState({
+            counter: state => state.counter
+        })
     }
 </script>
